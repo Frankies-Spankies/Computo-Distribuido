00e5d81a2b992a8a0892ca016d1b0be95151b5fc
Francisco Gamez <franciscogamezdg@gmail.com>
2020-12-04 21:28:04 -0600

    CD-21 CD-28 CD-23 #time 3h  #comment se cambio de lugar y se encapsulo la inicializacion de variable slice_size en nodos esclavos, ahora se hace en el setup

--- Documentos/Practicas/Practica2/Practica2.c
+++ Documentos/Practicas/Practica2/Practica2.c
@@ -22,7 +22,7 @@ int *arr_index;
 int leng;
 int *array;
 int index_wanted;
-int module=-1;
+int module = -1;
 
 //Implementacion ============>
 void inicializa()
@@ -33,28 +33,35 @@ void inicializa()
 
 void initiliaze_slave()
 {
-    wanted = -1;
-    slice_size = -1;
-    arr_wanted = NULL;
-    indx = -1;
-    arr_index = NULL;
+    MPI_Recv(&slice_size, 1, MPI_INT, 0, SLICE_SIZE, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    arr_wanted = (int *)malloc((slice_size) * sizeof(int));
+    arr_index = (int *)malloc((slice_size) * sizeof(int));
+    for (int i = 0; i < slice_size; i++)
+    {
+        arr_index[i] = -1;
+    } // inicializa todo arr_index a -1
 }
 
 void initiliaze_master(char **argv)
 {
     leng = (*argv[1] - '0');
     array = (int *)malloc((leng) * sizeof(int));
-    index_wanted = rand()%leng;
-    module = (leng)%(size-1);
+    index_wanted = rand() % leng;
+    module = (leng) % (size - 1);
 
     if (module != 0)
     {
         indx = module;
     }
 
-    slice_size = (leng - module) / (size-1);
+    slice_size = (leng - module) / (size - 1);
+
+    
+
+    arr_wanted =(int *)malloc((slice_size) * sizeof(int));
 
     int arr_result[leng][slice_size]; // TODO PONER EN OTRA PARTE
+
     for (int f = 0; f < leng; f++)
     {
         for (int c = 0; c < slice_size; c++)
@@ -62,15 +69,14 @@ void initiliaze_master(char **argv)
             arr_result[f][c] = -1;
         }
     }
-}
 
-void fill_arr_wanted()
-{
-    for (int j = indx; j <= indx + slice_size; j++)
+
+    for (int  i = 1; i < size; i++)
     {
-        arr_wanted[j] = array[j];
+        MPI_Send(&slice_size, 1, MPI_INT, i, SLICE_SIZE, MPI_COMM_WORLD);
     }
 }
+
 
 void data_search()
 {
@@ -85,35 +91,38 @@ void data_search()
     }
 }
 
+void fill_arr_wanted()
+{
+    for (int j = 0; j < slice_size; j++)
+    {
+        arr_wanted[j] = array[indx+j];
+    }
+}
+
 void send_data_to_search()
 {
-    for (int i = 1; i <= size; i++)
+    for (int i = 1; i < size; i++)
     {
         fill_arr_wanted();
         MPI_Send(&indx, 1, MPI_INT, i, INDEX, MPI_COMM_WORLD);
-        MPI_Send(&slice_size, 1, MPI_INT, i, SLICE_SIZE, MPI_COMM_WORLD);
         MPI_Send(&arr_wanted, slice_size, MPI_INT, i, SLICE, MPI_COMM_WORLD);
         indx += slice_size;
     }
-    //Busqueda individual en con residuos en mater
+    //Busqueda individual en con residuos en master
     for (int l = 0; l < module; l++)
     {
         arr_wanted[l] = array[l];
     }
     indx = 0;
+    slice_size = module;
 }
 
 void recive_data_to_search()
 {
     MPI_Recv(&indx, 1, MPI_INT, 0, INDEX, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    MPI_Recv(&slice_size, 1, MPI_INT, 0, SLICE_SIZE, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    arr_wanted = (int *)malloc((slice_size) * sizeof(int));
-    arr_index = (int *)malloc((slice_size) * sizeof(int));
-    for (int i = 0; i < slice_size; i++)
-    {
-        arr_index[i] = -1;
-    } // inicializa todo arr_index a -1
+
     MPI_Recv(&arr_wanted, slice_size, MPI_INT, 0, SLICE, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    //printf("algo: %d", arr_wanted[0]);
 }
 
 int distributed_search(int argc, char **argv)
@@ -131,7 +140,8 @@ int distributed_search(int argc, char **argv)
 
 //Tests ===============>
 
-void test_initiliaze_master(char **argv){
+void test_initiliaze_master(char **argv)
+{
     if (rank == 0)
     {
         printf("\n####test_initiliaze_master####\n");
@@ -145,7 +155,6 @@ void test_initiliaze_master(char **argv){
         printf("\nmodule: %d", module);
         printf("\nindx: %d\n", indx);
     }
-    
 }
 
 void test_data_search(char **argv)
@@ -164,6 +173,45 @@ void test_data_search(char **argv)
     }
 }
 
+void test_send_recive_data_to_search(char **argv)
+{
+    if (rank == 0)
+    {
+
+        initiliaze_master(argv);
+        printf("Enviando slice_size");
+    }else
+    {
+        initiliaze_slave();
+        printf("Reciviendo slice_size");
+    }
+    
+    
+    if (rank == 0)
+    {
+        //given
+        data_search();
+        send_data_to_search();
+/*         printf("\nnodo: %d\n", rank);
+        printf("\nindx: %d\n", indx);
+        printf("\nslice_size: %d\n", slice_size); */
+    }
+    else
+    {
+        //given
+        recive_data_to_search();
+        //then
+        printf("\nnodo: %d\n", rank);
+        printf("\nindx: %d\n", indx);
+        printf("\nslice_size: %d\n", slice_size);
+        printf("\n=====>Arr_wanted ");
+        printf("algo: %d", arr_wanted[1]);
+        for (int i = 0; i < slice_size; i++)
+        {
+            //printf(" %d", arr_wanted[i]);
+        }
+    }
+}
 
 int test(int argc, char **argv)
 {
@@ -171,8 +219,8 @@ int test(int argc, char **argv)
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     //inicia prueba
-    test_data_search(argv);
-    //f^un de prueba
+    test_send_recive_data_to_search(argv);
+    //fun de prueba
 
     MPI_Finalize();
     return 0;
